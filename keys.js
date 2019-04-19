903d2beb1b578bf70658e6d3cd52908e62e1a5cc
RickN123 <nicolai.richard@gmail.com>
2019-04-19 14:28:30 -0400

    js setup

--- keys.js
+++ keys.js
@@ -4,3 +4,9 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsintown = {
+    id: process.env.BANDSINTOWN_ID,
+    secret: process.env.BANDSINTOWN_SECRET
+
+};
--- liri.js
+++ liri.js
@@ -3,3 +3,7 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
+var bandsintown = new bandsintown(keys.bandsintown);
+
+
+
