c4f596899b5053de60ecc9d4c5016f4082a8b917
RickN123 <nicolai.richard@gmail.com>
2019-04-23 15:55:17 -0400

    Imdb

--- keys.js
+++ keys.js
@@ -4,9 +4,3 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
-
-exports.bandsintown = {
-    id: process.env.BANDSINTOWN_ID,
-    secret: process.env.BANDSINTOWN_SECRET
-
-};
--- liri.js
+++ liri.js
@@ -3,36 +3,54 @@ var keys = require("./keys.js");
 var fs = require("fs");
 var inquirer = require("inquirer");
 var axios = require("axios");
-
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
+var search = process.argv[2];
+var artist = process.argv.slice(3).join(" ").toLowerCase();
+console.log("Search:" + search);
+console.log("Artist:" + artist);
 
-var bandsintown = new bandsintown(keys.bandsintown);
+if (search == "concert-this") {
 
-function concert-this(){
-
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            console.log("Name:" + response.data[0].lineup[0]);
+            console.log("Venue Location:" + response.data[0].venue.name);
+            console.log("Date:" + moment(response.data[0].datetime).format('MM/DD/YYYY'));
+        }
+    ).catch(function (err) {
+        console.log(err)
+    })
 
 
 }
 
+if (search == "movie-this") {
 
 
-axios.get("http://www.omdbapi.com/?t=" + movie + "&ye&plot=short&apikey=trilogy").then(
-    function (response) {
-        console.log("Name:" + response.data.Title);
-        console.log("Release Year:" + response.data.Year);
-        console.log("IMDB rating is:" + response.data.imdbRating);
-        console.log("Rotten Tomatoes rating is: " + response.data.rottentomatoesrating);
-        console.log("Country Produced: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-    }
-);
-
-inquirer
-    .prompt([
-        {
-            type: "input"
-
+    axios.get("http://www.omdbapi.com/?t=" + artist + "&ye&plot=short&apikey=trilogy").then(
+        function (response) {
+            console.log(response);
+            console.log("Name:" + response.data.Title)
+            console.log("Release Year:" + response.data.Year);
+            console.log("IMDB rating is:" + response.data.imdbRating);
+            console.log("Rotten Tomatoes rating is: " + response.data.Ratings[0].Value);
+            console.log("Country Produced: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
         }
-    ])
+    ).catch(function (err) {
+        console.log(err)
+    })
+}
+
+
+// inquirer
+//     .prompt([
+//         {
+//             type: "input"
+
+//         }
+//     ])
