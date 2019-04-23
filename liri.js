02880df7e9a9c42b78c552fd4ab377a2ea1ffc82
RickN123 <nicolai.richard@gmail.com>
2019-04-22 20:12:26 -0400

    movie

--- liri.js
+++ liri.js
@@ -1,9 +1,22 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var fs = require("fs");
+var inquirer = require("inquirer");
+var axios = require("axios");
 
 var spotify = new Spotify(keys.spotify);
 
 var bandsintown = new bandsintown(keys.bandsintown);
 
+axios.get("http://www.omdbapi.com/?t=" + movie + "&ye&plot=short&apikey=trilogy").then(
+    function (response) {
+        console.log("The movie's rating is:" + response.data.imdbRating);
+)
 
+inquirer
+    .prompt([
+        {
+            type: "input"
 
+        }
+    ])
