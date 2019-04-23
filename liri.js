1264e5350473a8a3ee28370cb17d5f31963102f6
RickN123 <nicolai.richard@gmail.com>
2019-04-23 18:35:22 -0400

    updated readMe

--- liri.js
+++ liri.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var fs = require("fs");
-var inquirer = require("inquirer");
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
